af62020c484b6b4d81657342ce45ac9b914f6e63
zaid ragab <zragab@lcs.on.ca>
2023-01-24 08:46:13 -0500

    Initial Commit

---
+++ QUadricticFomrla/QUadricticFomrlaApp.swift
@@ -0,0 +1,17 @@
+//
+//  QUadricticFomrlaApp.swift
+//  QUadricticFomrla
+//
+//  Created by Zaid Ragab on 2023-01-24.
+//
+
+import SwiftUI
+
+@main
+struct QUadricticFomrlaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
